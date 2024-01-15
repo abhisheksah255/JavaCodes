51b03a8cd737ebac659b25bca3a83b2a5db75149
abhisheksah255 <abhisheksah255@gmail.com>
2024-01-15 20:23:13 +0530

    Example of Encapsulation

--- Encapsulation/Demo.java
+++ Encapsulation/Demo.java
@@ -1,8 +1,29 @@
 package JavaCodes.Encapsulation;
 
+class Human{
+    private int age;
+    private String name;
+
+
+public int getAge(){
+    return age;
+}
+public String getName(){
+    return name;
+}
+public void setAge(int age) {
+    this.age = age;
+}
+public void setName(String name) {
+    this.name = name;
+}
+    
+}
 public class Demo {
     public static void main(String[] args) {
-        
+        Human obj = new Human();
+
+        System.out.println(obj.getAge() + " : " + obj.getName());
     }
     
 }
