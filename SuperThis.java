ccfcfc836ff5a43c7d3313c391bafded6e9a4766
abhisheksah255 <abhisheksah255@gmail.com>
2024-01-19 16:51:04 +0530

    Add Parametrized constructor in class B and try to call by creating obj

--- SuperThis.java
+++ SuperThis.java
@@ -3,15 +3,19 @@ class A{
     public A(){
         System.out.println("This is the value of A");
     }
+
 }
 class B extends A{
     public B(){
         System.out.println("This is the value of B");
     }
+    public B(int x){
+        System.out.println("This is the value of B in parameterized constructor");
+    }
 }
 public class SuperThis {
     public static void main(String a[]) {
-     B obj =new B(); 
+     B obj =new B(5); 
         
     }
 }
