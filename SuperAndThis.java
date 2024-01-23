875f215e603c7144d1ffcd37a61e1d8b0a48eba4
abhisheksah255 <abhisheksah255@gmail.com>
2024-01-23 17:03:01 +0530

    Add super keywords and check the difference in the code

--- SuperAndThis.java
+++ SuperAndThis.java
@@ -1,18 +1,28 @@
 class A 
 {
 public A() {
+	super();
 	System.out.println("This is class A Constructor");
 }	
+public A(int y) {
+	super();
+	System.out.println("This is the Patametrized A contructor");
+}
 }
 class B extends A {
 	public B() {
+		super();
 		System.out.println("This is class B Constructor");
+	}
+	public B(int x) {
+		super(x);
+		System.out.println("This is the Patametrized B contructor");
 	}
 }
 public class SuperAndThis {
 
 	public static void main(String[] args) {
-	B obj=new B();
+	B obj=new B(5);
 
 	}
 
