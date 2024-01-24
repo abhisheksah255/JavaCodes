d35b184af8e009d41b51871e4d7323b08c5cceeb
abhisheksah255 <abhisheksah255@gmail.com>
2024-01-24 13:15:39 +0530

    Example of Inheritance

---
+++ Inheritance/AdvanceCal.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class AdvanceCal extends Calculator {
+	public int mul(int a, int b) {
+		return a*b;
+	}
+
+	public int div(int a, int b) {
+		return a/b;
+	}
+}
---
+++ Inheritance/Demo.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AdvanceCal ab=new AdvanceCal();
+		int p=ab.add(5, 6);
+		int q=ab.sub(16, 12);
+		int r=ab.mul(2, 5);
+		int s=ab.div(54, 5);
+
+		System.out.println(p +" " +q + " " +r+ " " +s);
+	}
+
+}
